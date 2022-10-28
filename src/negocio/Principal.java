166ac1ba81d555ce6b6ad4dae3439dfac34d471e
Iñigo Lorite Lobato <inigo.lorite.lobato@opendeusto.es>
2022-10-28 15:55:14 +0200

    Clase Principal con main para probar los métodos de la clase GestorBD

---
+++ src/negocio/Principal.java
@@ -0,0 +1,21 @@
+package negocio;
+
+import datos.GestorBD;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		GestorBD gestor = new GestorBD();
+		
+		// creacion
+		
+		// insercion
+		
+		// modificacion
+		
+		// borrado
+		
+	}
+	
+}
