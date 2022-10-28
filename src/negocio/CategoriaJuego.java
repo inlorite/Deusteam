622eb6b37355e0188050f3aee21b41eb327f8eae
AdrianMarTej <adrian.martinez.t@opendeusto.es>
2022-10-28 13:13:03 +0200

    Categoria Juego

--- src/negocio/CategoriaJuego.java
+++ src/negocio/CategoriaJuego.java
@@ -1,5 +1,6 @@
 package negocio;
 
 public enum CategoriaJuego {
-
+	Indie, Action, Adventure, Casual, Simulation, Fantasy, Singleplayer, Multiplayer, Arcade, Terror,
+	PvP, PvE, Hardcore, Survival, Horror, Zombies, MMORPG, RPG, Driving;
 }
