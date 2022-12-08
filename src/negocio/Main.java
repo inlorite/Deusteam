66cbc17f9dfb00ee6c185efffc2f31545b4554b7
Iñigo Lorite <inigo.lorite.lobato@opendeusto.es>
2022-12-08 18:55:00 +0100

    Banners

--- src/negocio/Main.java
+++ src/negocio/Main.java
@@ -107,26 +107,50 @@ public class Main {
 		
 		Game game = new Game();
 		game.setId(1);
-		game.setName("binding of yeray");
+		game.setName("Elden Ring");
 		game.setCompany("company");
 		game.setPegi(Pegi.PEGI12);
 		game.setGenre1(GameGenre.Action);
 		game.setGenre2(GameGenre.Adventure);
 		game.setPrice(10.0);
-		game.setDescription("description1");
-		game.setImgLink("link");
+		game.setDescription("THE NEW FANTASY ACTION RPG. Rise, Tarnished, and be guided by grace to brandish the power of the Elden Ring and become an Elden Lord in the Lands Between.");
+		game.setImgLink("data/game_banners/elden_ring");
 		games.add(game);
 		
 		game = new Game();
 		game.setId(2);
-		game.setName("space adrian");
+		game.setName("Fifa 23");
 		game.setCompany("company");
 		game.setPegi(Pegi.PEGI7);
 		game.setGenre1(GameGenre.Action);
 		game.setGenre2(GameGenre.Arcade);
 		game.setPrice(12.0);
-		game.setDescription("description2");
-		game.setImgLink("link");
+		game.setDescription("Experience the excitement of the biggest tournament in football with EA SPORTS™ FIFA 23 and the men’s FIFA World Cup™ update, available on November 9 at no additional cost!");
+		game.setImgLink("data/game_banners/fifa_23");
+		games.add(game);
+		
+		game = new Game();
+		game.setId(3);
+		game.setName("Persona 5");
+		game.setCompany("company");
+		game.setPegi(Pegi.PEGI7);
+		game.setGenre1(GameGenre.Action);
+		game.setGenre2(GameGenre.Arcade);
+		game.setPrice(12.0);
+		game.setDescription("Don the mask and join the Phantom Thieves of Hearts as they stage grand heists, infiltrate the minds of the corrupt, and make them change their ways!");
+		game.setImgLink("data/game_banners/persona_5");
+		games.add(game);
+		
+		game = new Game();
+		game.setId(4);
+		game.setName("Vampire Survivors");
+		game.setCompany("company");
+		game.setPegi(Pegi.PEGI7);
+		game.setGenre1(GameGenre.Action);
+		game.setGenre2(GameGenre.Arcade);
+		game.setPrice(12.0);
+		game.setDescription("Mow down thousands of night creatures and survive until dawn! Vampire Survivors is a gothic horror casual game with rogue-lite elements, where your choices can allow you to quickly snowball against the hundreds of monsters that get thrown at you.");
+		game.setImgLink("data/game_banners/vampire_survivors");
 		games.add(game);
 		
 		return games;
