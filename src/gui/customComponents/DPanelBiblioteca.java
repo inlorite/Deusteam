2b2ae7328f8046e632c6b8d81700ef5af96d9aad
Iñigo Lorite <inigo.lorite.lobato@opendeusto.es>
2022-12-10 16:22:08 +0100

    DPanelBiblioteca panel info loaded

--- src/gui/customComponents/DPanelBiblioteca.java
+++ src/gui/customComponents/DPanelBiblioteca.java
@@ -1,11 +1,14 @@
 package gui.customComponents;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Map;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import gui.VDeusteam;
 import gui.VLogin;
@@ -50,12 +53,14 @@ public class DPanelBiblioteca extends JPanel {
 		panelInstalados.setBorder(new TitledBorder("Juegos instalados"));
 		dlmJuegosInstalados = new DefaultListModel<>();
 		lJuegosInstalados = new JList<>(dlmJuegosInstalados);
+		lJuegosInstalados.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		panelInstalados.add(new JScrollPane(lJuegosInstalados), BorderLayout.CENTER);
 		
 		JPanel panelDisponibles = new JPanel(new BorderLayout());
 		panelDisponibles.setBorder(new TitledBorder("Juegos Disponibles"));
 		dlmJuegosDisponibles = new DefaultListModel<>();
 		lJuegosDisponibles = new JList<>(dlmJuegosDisponibles);
+		lJuegosDisponibles.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		panelDisponibles.add(new JScrollPane(lJuegosDisponibles), BorderLayout.CENTER);
 		
 		Map<Game, Boolean> userGames = VLogin.dbManager.obtainDataPropertyUserInstalledGames(VLogin.loggedUser.getId());
@@ -68,6 +73,38 @@ public class DPanelBiblioteca extends JPanel {
 			}
 		}
 		
+		ArrayList<JList<Game>> lists = new ArrayList<>();
+		lists.add(lJuegosDisponibles);
+		lists.add(lJuegosInstalados);
+		
+		ListSelectionListener lsl = new ListSelectionListener() {
+			
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				for (JList<Game> list : lists) {
+					if (list != e.getSource()) {
+						list.clearSelection();
+					}
+					
+					if (list == e.getSource()) {
+						if (!e.getValueIsAdjusting()){
+							Game g = list.getSelectedValue();
+							
+							ImageIcon ii = new ImageIcon("data/game_banners/" + g.getId() + ".jpg");
+							lBanner.setIcon(ii);
+							lInfo.setText(g.getDescription());
+							
+							revalidate();
+							repaint();
+						}
+					}
+				}
+			}
+		};
+		
+		lJuegosInstalados.addListSelectionListener(lsl);
+		lJuegosDisponibles.addListSelectionListener(lsl);
+		
 		panel.add(panelInstalados);
 		panel.add(panelDisponibles);
 		
@@ -77,7 +114,7 @@ public class DPanelBiblioteca extends JPanel {
 	public JPanel getCenterInfoJuego() {
 		JPanel panel = new JPanel(new BorderLayout(VDeusteam.GAP, VDeusteam.GAP));
 		
-		lBanner = new JLabel("banner");
+		lBanner = new JLabel("");
 		lInfo = new JLabel("info");
 		bInstalar = new JButton("Instalar");
 		
